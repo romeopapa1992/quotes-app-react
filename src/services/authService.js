355486574d78428951cdb4308faaf139f9ce6f9d
Rafal <rafal.proski.programista@gmail.com>
2025-01-12 15:44:43 +0100

    https url added

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://api.quotes.romeopapa1992.org";
+const API_URL = "https://api.quotes.romeopapa1992.org";
 
 export const getAccessToken = () => localStorage.getItem("token");
 export const getRefreshToken = () => localStorage.getItem("refreshToken");
